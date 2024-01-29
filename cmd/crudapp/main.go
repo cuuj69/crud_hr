7534c3943397596aae8d19fff7a8c2b04927535d
cuuj69 <mensahjefferson69@gmail.com>
2024-01-29 15:19:34 +0000

    Modified:added_env

--- cmd/crudapp/main.go
+++ cmd/crudapp/main.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/cuuj69/crud_hr/internal/employee"
 	"github.com/cuuj69/crud_hr/web/routes"
+	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 
 	"gorm.io/driver/mysql"
+	"os"
 )
 
 
@@ -17,6 +19,11 @@ var db *gorm.DB
 func main() {
 	fmt.Println("HR Employee Management System")
 
+	//load .env
+
+	if err := godotenv.Load(); err != nil{
+		panic("Error loading .env file")
+	}
 
 	initDB()
 
@@ -43,7 +50,17 @@ func initDB(){
 
 	var err error
 
-	dsn := "mensah:@forum500A@tcp(127.0.0.1:3306)/hr?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "mensah:@forum500A@tcp(127.0.0.1:3306)/hr?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_PORT"),
+        os.Getenv("DB_NAME"),
+    )
+
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
